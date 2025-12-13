346e32e9df8fd9e3f90d6108f7fa9cd1e4e58336
KrushnaKapare <krushnakapare25@gmail.com>
2025-12-13 23:38:19 +0530

    python

---
+++ demo1.py
@@ -0,0 +1,22 @@
+# temperature = 0.1 + 0.2
+# if temperature == 0.3:
+#     print("OK")
+# else:
+#     print("Mismatch")
+
+# if round(0.1+0.2,1)==0.3:
+#     print("OK")
+# else:
+#     print("Mismatch")
+
+# if abs((0.1+0.2) - 0.3) < 0.00001:
+#     print("OK2")
+# else:
+#     print("Mismatch")
+
+from decimal import Decimal
+
+a = Decimal("0.1")
+b = Decimal("0.2")
+
+print(a+b)
